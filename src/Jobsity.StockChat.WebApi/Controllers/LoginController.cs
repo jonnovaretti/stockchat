e69fcf027d1ec22f707093187e56284744144c8e
Jonathan Novaretti <jonathan.novaretti@xpi.com.br>
2021-05-15 19:47:59 -0300

    Add login controller integrated tests

--- src/Jobsity.StockChat.WebApi/Controllers/LoginController.cs
+++ src/Jobsity.StockChat.WebApi/Controllers/LoginController.cs
@@ -2,6 +2,8 @@
 using Jobsity.StockChat.WebApi.Services;
 using Jobsity.StockChat.WebApi.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace Jobsity.StockChat.WebApi.Controllers
 {
@@ -11,25 +13,35 @@ namespace Jobsity.StockChat.WebApi.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly ITokenService _tokenService;
+        private readonly ILogger<LoginController> _logger;
 
-        public LoginController(IUserRepository userRepository, ITokenService tokenService)
+        public LoginController(IUserRepository userRepository, ITokenService tokenService, ILogger<LoginController> logger)
         {
             _userRepository = userRepository;
             _tokenService = tokenService;
+            _logger = logger;
         }
 
         [HttpPost]
         [Route("login")]
         public ActionResult Authenticate([FromBody] UserRequest request)
         {
-            var userFound = _userRepository.Get(request.Username, request.Password);
+            try
+            {
+                var userFound = _userRepository.Get(request.Username, request.Password);
 
-            if (userFound is null)
-                return NotFound(new Response("Invalid credentials"));
+                if (userFound is null)
+                    return Unauthorized(new Response("Invalid credentials"));
 
-            var token = _tokenService.GenerateToken(userFound.Username, userFound.Role);
+                var token = _tokenService.GenerateToken(userFound.Username, userFound.Role);
 
-            return Ok(new Response<AuthenticateResponse>(new AuthenticateResponse(userFound.Username, token)));
+                return Ok(new Response<AuthenticateResponse>(new AuthenticateResponse(userFound.Username, token)));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Internal error on {LoginController}", nameof(LoginController));
+                return BadRequest();
+            }
         }
     }
 }
