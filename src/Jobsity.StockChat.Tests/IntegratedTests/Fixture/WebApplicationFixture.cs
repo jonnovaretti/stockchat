f83f5d183729a65263842a2d7ff767ac12bc541e
Jonathan Novaretti <jonathan.novaretti@xpi.com.br>
2021-05-15 12:43:08 -0300

    Add fixture for service integration test

---
+++ src/Jobsity.StockChat.Tests/IntegratedTests/Fixture/WebApplicationFixture.cs
@@ -0,0 +1,26 @@
+﻿using Jobsity.StockChat.WebApi.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jobsity.StockChat.Tests.IntegratedTests.Fixture
+{
+    public class WebApplicationFixture<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        private readonly IConfiguration _configuration;
+
+        public WebApplicationFixture(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddAuthentication(_configuration);
+            });
+        }
+    }
+}
---
+++ src/Jobsity.StockChat.Tests/IntegratedTests/Services/TokenServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Jobsity.StockChat.WebApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jobsity.StockChat.Tests.IntegratedTests.Services
+{
+    public class TokenServiceTests 
+    {
+        private readonly ITokenService _tokenService;
+
+        public TokenServiceTests()
+        {
+            
+        }
+    }
+}
