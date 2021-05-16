ca732863e6b2a29f9174c66ef1cd12b101c802ab
Jonathan Novaretti <jonathan.novaretti@xpi.com.br>
2021-05-16 08:32:04 -0300

    Add docker-compose rabbitmq

--- src/Jobsity.StockChat.WebApi/Hubs/HubService.cs
+++ src/Jobsity.StockChat.WebApi/Hubs/HubService.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Jobsity.StockChat.Application.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Jobsity.StockChat.WebApi.Hubs
@@ -9,6 +11,12 @@ namespace Jobsity.StockChat.WebApi.Hubs
     public class HubService : Hub
     {
         private const string ClientMethodName = "receiveMessageFromServer";
+        private readonly IMessageAnalyserService _messageAnalyserService;
+
+        public HubService(IMessageAnalyserService messageAnalyserService)
+        {
+            _messageAnalyserService = messageAnalyserService;
+        }
 
         public async Task SendMessage(string user, string message)
         {
@@ -17,6 +25,8 @@ namespace Jobsity.StockChat.WebApi.Hubs
 
         public async Task ReceiveMessageFromClient(string message)
         {
+            var commands = _messageAnalyserService.GetCommands(message);
+
             await SendMessageToClient(Context.UserIdentifier, message);
         }
 
