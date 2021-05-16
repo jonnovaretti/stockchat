2b612df92cd74804a04ba444ac4cb8d9b4e5dc13
Jonathan Novaretti <jonathan.novaretti@xpi.com.br>
2021-05-15 21:23:17 -0300

    Set client communcation with hub service

--- src/Jobsity.StockChat.WebApi/Hubs/HubService.cs
+++ src/Jobsity.StockChat.WebApi/Hubs/HubService.cs
@@ -5,6 +5,14 @@ namespace Jobsity.StockChat.WebApi.Hubs
 {
     public class HubService : Hub
     {
-        
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("receiveMessage", user, message);
+        }
+
+        public async Task ReceiveMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("receiveMessage", user, message);
+        }
     }
 }
