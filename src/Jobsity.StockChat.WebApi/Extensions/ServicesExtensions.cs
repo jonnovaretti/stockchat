56a67cd6b6202f2c408025320247590813dbece0
Jonathan Novaretti <jonathan.novaretti@xpi.com.br>
2021-05-15 22:21:06 -0300

    Set hub jwt authentication

--- src/Jobsity.StockChat.WebApi/Extensions/ServicesExtensions.cs
+++ src/Jobsity.StockChat.WebApi/Extensions/ServicesExtensions.cs
@@ -1,11 +1,14 @@
 ﻿using Jobsity.StockChat.Application.Infrastructure.Repositories;
 using Jobsity.StockChat.Application.Services;
 using Jobsity.StockChat.Application.Settings;
+using Jobsity.StockChat.WebApi.Hubs;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Jobsity.StockChat.WebApi.Extensions
 {
@@ -23,22 +26,38 @@ namespace Jobsity.StockChat.WebApi.Extensions
             var key = Encoding.ASCII.GetBytes(authSettings.Secret);
 
             services.AddSingleton<IAuthSetting>(authSettings);
+            services.AddSingleton<IUserIdProvider, UserIdProvider>();
 
             services.AddAuthentication(x =>
             {
                 x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             })
-            .AddJwtBearer(x =>
+            .AddJwtBearer(options =>
             {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuerSigningKey = true,
                     IssuerSigningKey = new SymmetricSecurityKey(key),
                     ValidateIssuer = false,
                     ValidateAudience = false
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hub"))
+                        {
+                            context.Token = accessToken;
+                        }
+
+                        return Task.CompletedTask;
+                    }
                 };
             });
         }
--- src/Jobsity.StockChat.WebApi/Hubs/HubService.cs
+++ src/Jobsity.StockChat.WebApi/Hubs/HubService.cs
@@ -1,18 +1,28 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
 namespace Jobsity.StockChat.WebApi.Hubs
 {
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class HubService : Hub
     {
+        private const string ClientMethodName = "receiveMessageFromServer";
+
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("receiveMessage", user, message);
+            await SendMessageToClient(user, message);
         }
 
-        public async Task ReceiveMessage(string user, string message)
+        public async Task ReceiveMessageFromClient(string message)
         {
-            await Clients.All.SendAsync("receiveMessage", user, message);
+            await SendMessageToClient(Context.UserIdentifier, message);
+        }
+
+        private async Task SendMessageToClient(string user, string message)
+        {
+            await Clients.All.SendAsync(ClientMethodName, user, message);
         }
     }
 }
